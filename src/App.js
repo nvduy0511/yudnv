100de8075968609ec215285f125f353c5dba8d80
nvduy0511 <nvduy.k19hutech@gmail.com>
2022-07-06 17:06:07 +0700

    done conversation-list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'react';
+import socketIOClient from 'socket.io-client';
 import './App.css';
+const host = 'http://localhost:3001';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [mess, setMess] = useState([]);
+    const [message, setMessage] = useState('');
+    const [id, setId] = useState();
+
+    const socketRef = useRef();
+    const messagesEnd = useRef();
+
+    useEffect(() => {
+        socketRef.current = socketIOClient.connect(host);
+
+        socketRef.current.on('getId', (data) => {
+            setId(data);
+        });
+
+        socketRef.current.on('sendDataServer', (dataGot) => {
+            setMess((oldMsgs) => [...oldMsgs, dataGot.data]);
+            scrollToBottom();
+        });
+
+        return () => {
+            socketRef.current.disconnect();
+        };
+    }, []);
+
+    const sendMessage = () => {
+        if (message !== null) {
+            const msg = {
+                content: message,
+                id: id,
+            };
+            socketRef.current.emit('sendDataClient', msg);
+            setMessage('');
+        }
+    };
+
+    const scrollToBottom = () => {
+        messagesEnd.current.scrollIntoView({ behavior: 'smooth' });
+    };
+
+    const renderMess = mess.map((m, index) => (
+        <div key={index} className={`${m.id === id ? 'your-message' : 'other-people'} chat-item`}>
+            {m.content}
+        </div>
+    ));
+
+    const handleChange = (e) => {
+        setMessage(e.target.value);
+    };
+
+    const onEnterPress = (e) => {
+        if (e.keyCode === 13 && e.shiftKey === false) {
+            sendMessage();
+        }
+    };
+
+    return (
+        <div class="box-chat">
+            <div class="box-chat_message">
+                {renderMess}
+                <div style={{ float: 'left', clear: 'both' }} ref={messagesEnd}></div>
+            </div>
+
+            <div class="send-box">
+                <textarea
+                    value={message}
+                    onKeyDown={onEnterPress}
+                    onChange={handleChange}
+                    placeholder="Nhập tin nhắn ..."
+                />
+                <button onClick={sendMessage}>Send</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import Login from './components/Login';
+import Message from './components/Message';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+export default function AppTest() {
+    return (
+        <>
+            <Message />
+        </>
+    );
+}
